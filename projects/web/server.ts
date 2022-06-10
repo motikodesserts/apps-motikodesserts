82cb371b6c81a6eabbd2a3d5eeebb59267491ff6
ioan <ioanbeilic@gmail.com>
2022-06-10 15:25:27 +0200

    update server ts

--- projects/web/server.ts
+++ projects/web/server.ts
@@ -64,8 +64,12 @@ function run(): void {
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
 const moduleFilename = (mainModule && mainModule.filename) || '';
+/*
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
 }
+*/
+
+run();
 
 export * from './src/main.server';
