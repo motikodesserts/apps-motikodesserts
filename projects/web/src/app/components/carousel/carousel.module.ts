cb532cb9837b28e32adc319b07ffb62ce3dde283
ioan <ioanbeilic@gmail.com>
2022-06-22 20:43:02 +0200

    text to slide

--- projects/web/src/app/components/carousel/carousel.module.ts
+++ projects/web/src/app/components/carousel/carousel.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from './carousel.component';
-import { SvgComponent } from './svg/svg.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [CarouselComponent, SvgComponent],
-  imports: [CommonModule],
+  declarations: [CarouselComponent],
+  imports: [CommonModule, RouterModule],
   exports: [CarouselComponent],
 })
 export class CarouselModule {}
