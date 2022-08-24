ff2cbfdc0640c0c421623e74cdb7ad6629033164
ioan <ioanbeilic@gmail.com>
2022-08-24 18:49:46 +0200

    parners

--- projects/web/src/app/pages/partners/partners.component.ts
+++ projects/web/src/app/pages/partners/partners.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-partners',
   templateUrl: './partners.component.html',
-  styleUrls: ['./partners.component.scss']
+  styleUrls: ['./partners.component.scss'],
 })
 export class PartnersComponent implements OnInit {
+  headerImg = Math.floor(Math.random() * 34);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
